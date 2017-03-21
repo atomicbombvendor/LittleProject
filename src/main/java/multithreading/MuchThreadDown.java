0f46b24a4e9cc6b44d42fed2df1911703fb7f843
MSDOMAIN1\eli9 <eddie.li@morningstar.com>
2017-03-21 17:03:03 +0800

    create branches dev

--- src/main/java/multithreading/MuchThreadDown.java
+++ src/main/java/multithreading/MuchThreadDown.java
@@ -4,4 +4,7 @@ package multithreading;
  * Created by eli9 on 3/21/2017.
  */
 public class MuchThreadDown {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
