34c045742b770f0487f72fe4d182eca0db1fdaf8
xiaobai <atomicbombvendor@gmail.com>
2017-03-27 22:05:54 +0800

    Solve problem: can't find properties file

--- src/test/java/Properties/Process/TableStructureServiceImplTest.java
+++ src/test/java/Properties/Process/TableStructureServiceImplTest.java
@@ -1,18 +1,22 @@
 package Properties.Process;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 /**
  * Created by eli9 on 3/27/2017.
  */
 public class TableStructureServiceImplTest {
+    @org.junit.jupiter.api.Test
+    void getSql1() {
+    }
+
     @Test
     public void getTableStructureEntity() throws Exception {
     }
 
     @Test
     public void getSql() throws Exception {
-        String root =Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String root =Thread.currentThread().getContextClassLoader().getResource("").getPath().toString();
         String path = root + "cdctables.properties";
         String tableName = "CentralEndData.cdc.dbo_PriceTrend_CT";
         TableStructureService tableStructureService = new TableStructureServiceImpl(path,tableName);
