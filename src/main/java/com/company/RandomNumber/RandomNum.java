6fbe4c2de8de37df9220f9c6d0bece0119272c32
xiaobai <atomicbombvendor@gmail.com>
2017-04-11 23:54:49 +0800

    1.Pom.xml: comment repositories 2.增加了随机数类；增加一个返回的结果满足条件就退出当前循环的测试类

---
+++ src/main/java/com/company/RandomNumber/RandomNum.java
@@ -0,0 +1,37 @@
+package com.company.RandomNumber;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static java.util.Arrays.asList;
+
+/**
+ * Created by ZZ on 2017/4/11.
+ */
+public class RandomNum {
+    public static int getRandom1To10(){
+        Random random = new Random();
+        int result = random.nextInt(10);// nextInt 0~n
+        return result+1;
+    }
+
+    public static int getRandom1To10ThrowException() throws Exception {
+        String n = String.valueOf(getRandom1To10());
+        String[] arr = new String[]{"2","4"};
+        String[] arr2 = new String[]{"11","12","13"};
+        System.out.println("random is "+n);
+        if(asList(arr).contains(n)){
+            return 200;
+        }else{
+            throw new Exception("Random is not in"+arr.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(getRandom1To10ThrowException());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/company/RandomNumber/conditionsExit.java
@@ -0,0 +1,60 @@
+package com.company.RandomNumber;
+
+/**
+ * Created by ZZ on 2017/4/11.
+ */
+
+/**
+ * 为了测试条件退出。当满足条件的时候，就退出，不然执行程序直到条件满足
+ */
+public class conditionsExit {
+
+    public static void main(String[] args) {
+        conditionsExit.exitWhenFillConditionFor();
+    }
+
+    /**
+     * 当条件满足是退出循环
+     */
+    public static void exitWhenFillCondition() {
+        boolean flag = true;
+        int result = 0;
+        while (flag) {
+            try {
+                result = RandomNum.getRandom1To10ThrowException();
+            } catch (Exception e) {
+
+            }
+            if (result == 200) {
+                flag = false;
+            }
+        }
+        if (flag == false) {
+            System.out.println("exit!");
+        }
+    }
+
+    public static void exitWhenFillConditionFor() {
+        int runTimes = 0;
+        int reRunTimes = 10;
+        int result = 0;
+        while (runTimes < reRunTimes) {
+            try {
+                result = RandomNum.getRandom1To10ThrowException();
+            } catch (Exception e) {
+
+            }
+            if (result == 200) {
+                break;
+            }
+            System.out.println("runTimes is "+runTimes);
+            runTimes++;
+        }
+
+        if (result == 200) {
+            System.out.println("200 OK!");
+        }else{
+            System.out.println("runTimes: " +runTimes+" should be more than 10 times!");
+        }
+    }
+}
