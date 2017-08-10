b553b2e94f26c60c73248351419f14239cd3d23c
atomicbombvendor <atomicbombvendor@gmail.com>
2017-08-10 17:53:07 +0800

    测试 Map ContainsKey 需要重写类的HashCode和Equals方法

--- src/main/java/com/company/Entity/Person.java
+++ src/main/java/com/company/Entity/Person.java
@@ -4,7 +4,7 @@ package com.company.Entity;
  * Created by eli9 on 4/17/2017.
  */
 //E表示的范型。表明这个类中可以有0到n个E类型的成员变量。E的类型在实例化时定义
-public class Person<T> {
+public class Person<T> extends Father{
     private String name;
     private int age;
     private T t;
@@ -16,6 +16,12 @@ public class Person<T> {
     public Person(String name, int age){
         this.name = name;
         this.age = age;
+    }
+
+    public Person(String name, int age, T t){
+        this.name = name;
+        this.age = age;
+        this.t = t;
     }
 
     public String getName() {
@@ -32,5 +38,25 @@ public class Person<T> {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person<?> person = (Person<?>) o;
+
+        if (age != person.age) return false;
+        if (name != null ? !name.equals(person.name) : person.name != null) return false;
+        return t != null ? t.equals(person.t) : person.t == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        result = 31 * result + (t != null ? t.hashCode() : 0);
+        return result;
     }
 }
---
+++ src/main/java/com/company/testCase/MapContainsKey.java
@@ -0,0 +1,64 @@
+package com.company.testCase;
+
+import com.company.Entity.ExchangeEntity;
+import com.company.Entity.Person;
+
+import java.util.*;
+
+/**
+ * Created by eli9 on 8/10/2017.
+ */
+public class MapContainsKey {
+
+    public static boolean testContainsKey(Person person){
+        Map<Person, String> map = new HashMap<>();
+        Person<String> p1 = new Person<>("name1", 11, "Value");
+        Person<String> p2 = new Person<>("name2", 11, "Value");
+        Person<String> p3 = new Person<>("name3", 11, "Value");
+
+        map.put(p1, "value1");
+        map.put(p2, "value2");
+        map.put(p3, "value3");
+
+        Set set = map.entrySet();
+        List<Map.Entry<Person, String>> list = new ArrayList<>(map.entrySet());
+        Set s = map.entrySet();
+        for (Object o : s) {
+            ((Map.Entry)o).getKey();
+        }
+        Person p = list.get(0).getKey();
+
+        return map.containsKey(person);
+    }
+
+    public static void main(String[] args) {
+        Person<String> t1 = new Person<>("name1", 11, "Value");
+        Person<Integer> t2 = new Person<>("name1", 11, 13);
+
+        System.out.println(testContainsKey(t1));
+        System.out.println(testContainsKey(t2));
+
+        ExchangeEntity entity = new ExchangeEntity();
+        entity.setCountryName("Name");
+        ExchangeEntity entity2 = new ExchangeEntity();
+        entity2.setCountryId("C1");
+
+        System.out.println(testContainsKey(entity));
+        System.out.println(testContainsKey(entity2));
+    }
+
+    public static boolean testContainsKey(ExchangeEntity entity){
+        Map<ExchangeEntity, String> map = new HashMap<>();
+        ExchangeEntity e1 = new ExchangeEntity();e1.setCountryId("C1");
+        ExchangeEntity e2 = new ExchangeEntity();e1.setCountryId("c2");
+        ExchangeEntity e3 = new ExchangeEntity();e1.setCountryId("c3");
+
+        System.out.println("Put1: "+map.put(e1, "value1"));
+        System.out.println("Put2: "+map.put(e2, "value2"));
+        System.out.println("Put3: "+map.put(e3, "value3"));
+
+        System.out.println("Put1: "+map.put(e1, "value4"));
+
+        return map.containsKey(entity);
+    }
+}
